4b85f9b9d0a856756752e1572492a5df2589f5b5
taine axel <axeltaine@live.fr>
2019-07-19 10:29:24 +0200

    suprim

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,4 +13,8 @@ class BooksController < ApplicationController
     Book.find(params[:id]).update title: params[:title]
     redirect_to "/books/#{params[:id]}"
   end
+  def destroy
+    Book.find(params[:id]).destroy
+    redirect_to "/books"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'contact' => 'pages#contact'
   patch 'books/:id' => 'books#update'
+  delete 'books/:id' => 'books#destroy'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
