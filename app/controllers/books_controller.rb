b5dfb5836a22136e8847369437034b7ed26c1681
taine axel <axeltaine@live.fr>
2019-07-19 10:09:40 +0200

    modif livre

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,16 @@
 class BooksController < ApplicationController
   def index
-    @les_livres = Book.all
+    @books = Book.all
   end
   def create
     Book.create title: params[:title]
     redirect_to "/books"
+  end
+  def show
+    @book = Book.find(params[:id])
+  end
+  def update
+    Book.find(params[:id]).update title: params[:title]
+    redirect_to "/books/#{params[:id]}"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   get 'books' => 'books#index'
-  post 'books' => 'books#create'
   get 'books/:id' => 'books#show'
+  post 'books' => 'books#create'
   root 'pages#home'
   get 'contact' => 'pages#contact'
+  patch 'books/:id' => 'books#update'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
