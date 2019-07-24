4887f62d415a1b5c6a3c37a37c03305e9278c42f
taine axel <axeltaine@live.fr>
2019-07-24 11:31:48 +0200

    modif route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  get 'books' => 'books#index'
-  get 'books/:id' => 'books#show'
-  post 'books' => 'books#create'
   root 'pages#home'
   get 'contact' => 'pages#contact'
-  patch 'books/:id' => 'books#update'
-  delete 'books/:id' => 'books#destroy'
+  resources :books, only: [:index, :create, :show, :update, :destroy]
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
