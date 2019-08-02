b04843c7f0c52394b0bef5b3def64bf2322e51b6
axeltaine <axeltaine@live.fr>
2019-08-02 15:37:55 +0200

    modif

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -8,7 +8,7 @@ function bookSearch(){
 
         success: function(data) {
             for(i = 0; i < data.items.length; i++){
-                results.innerHTML += "<h2>" + data.items[i].volumeInfo.title + "</h2>" + "<img src="+data.items[i].volumeInfo.imageLinks.smallThumbnail+">" + '<button id="ajout" type="button" class="btn btn-warning">' + "Ajouter" + "</button>"
+                results.innerHTML += "<h2>" + data.items[i].volumeInfo.title + "</h2>" + "<img src="+data.items[i].volumeInfo.imageLinks.smallThumbnail+">" + '<button id="ajout" type="button" class="btn btn-warning" onclick="bookAjout()" >' + "Ajouter" + "</button>"
             }
         },
 
@@ -17,3 +17,7 @@ function bookSearch(){
 }
 
 document.getElementById('button').addEventListener('click', bookSearch, false)
+
+function bookAjout(){
+    console.log('ok')
+}
